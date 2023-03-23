644a67255b3d8f6350597bc285cca08eec8d8433
SushmaSathish <sushmasathish.ngl@gmail.com>
2023-03-23 15:03:16 +0530

    gitogit

--- .vscode/GITSTART/MAIN.js
+++ .vscode/GITSTART/MAIN.js
@@ -1,3 +1,5 @@
 
+console.log("i love git");
 
-
+console.log("sushma");
+ 
---
+++ .vscode/main.js
@@ -0,0 +1,5 @@
+
+
+const ul = document.querySelector(".item");
+ul.firstElementChild.text.color ='#fcba03';
+ul.children[1].text.color ='yellow';
